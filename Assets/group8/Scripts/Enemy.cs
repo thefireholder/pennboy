b0a060311d5d531a98de38372c37ab11115d8546
Sangjoon Lee <aaccjjt@hotmail.com>
2024-09-18 05:01:50 -0400

    started working on enemy

---
+++ Assets/group8/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ClimbUp()
+    {
+
+    }
+
+    void FlyAway()
+    {
+        // activated when affected by bomb; should pretty much fly away in death
+
+    }
+}

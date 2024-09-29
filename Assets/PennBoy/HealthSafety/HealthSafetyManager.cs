595c64a4431f05ae8863585a54a07db4a6a1f448
Charles Wang <czw@seas.upenn.edu>
2024-09-29 03:19:15 -0400

    simple pennboy logo anim

--- Assets/PennBoy/HealthSafety/HealthSafetyManager.cs
+++ Assets/PennBoy/HealthSafety/HealthSafetyManager.cs
@@ -13,10 +13,16 @@ public class HealthSafetyManager : MonoBehaviour
     public PressAnyKeyBehavior pressAny;
 
     [Header("Logo Sequence Canvas")] public GameObject logoSequence;
+    public GameObject upgradeLogo;
+    public List<GameObject> letters;
 
     private bool polling;
 
     private void Awake() {
+#if UNITY_EDITOR
+        intro.SetActive(true);
+        logoSequence.SetActive(false);
+#endif
         pressAny.paused = true;
         repeatingBg.GetComponent<Image>().color = Color.black;
         foreach (var obj in texts) obj.GetComponent<CanvasGroup>().alpha = 0f;
@@ -50,7 +56,7 @@ public class HealthSafetyManager : MonoBehaviour
             StartCoroutine(Anim.Animate(0.7f, t => {
                 canvasGroup.alpha = t;
                 rectTrans.anchoredPosition =
-                    new Vector2(finalPos.x, Mathf.Lerp(initYPos, finalPos.y, Easing.EaseOutExpo(t)));
+                        new Vector2(finalPos.x, Mathf.Lerp(initYPos, finalPos.y, Easing.EaseOutExpo(t)));
             }));
 
             yield return new WaitForSeconds(0.1f);
@@ -65,11 +71,11 @@ public class HealthSafetyManager : MonoBehaviour
 
         if (Input.anyKey) {
             polling = false;
-            StartCoroutine(AnimateSequence());
+            StartCoroutine(AnimateIntroSequence());
         }
     }
 
-    private IEnumerator AnimateSequence() {
+    private IEnumerator AnimateIntroSequence() {
         pressAny.paused = true;
         pressAny.Flash();
 
@@ -84,8 +90,8 @@ public class HealthSafetyManager : MonoBehaviour
         var initScale = rectTransform.localScale;
         var finalScale = new Vector3(1.5f, 1.5f, 1.5f);
         StartCoroutine(Anim.Animate(0.7f, t => {
-            t = Easing.EaseInOutQuint(t);
-            rectTransform.localScale = Vector3.Lerp(initScale, finalScale, t);
+            var newT = Easing.EaseInOutQuint(t);
+            rectTransform.localScale = Vector3.Lerp(initScale, finalScale, newT);
         }));
 
         yield return new WaitForSeconds(0.35f);
@@ -97,7 +103,44 @@ public class HealthSafetyManager : MonoBehaviour
             bgImage.color = Color.Lerp(initBgCol, Color.white, t);
         });
 
+        StartCoroutine(AnimateLogoSequence());
+    }
+
+    private IEnumerator AnimateLogoSequence() {
+        // Setup animation, transition letters downwards and set opacities to zero
+        var letterRectTrans = letters.Select(letter => letter.GetComponent<RectTransform>());
+        var letterCanvasGroups = letters.Select(letter => letter.GetComponent<CanvasGroup>());
+        var letterAttrs = letterRectTrans.Zip(letterCanvasGroups, (a, b) => (a, b)).ToList();
+
+        upgradeLogo.GetComponent<CanvasGroup>().alpha = 0f;
+        foreach (var (rectTrans, canvasGroup) in letterAttrs) {
+            canvasGroup.alpha = 0f;
+            rectTrans.anchoredPosition = new Vector2(rectTrans.anchoredPosition.x, -170);
+        }
+
         intro.SetActive(false);
         logoSequence.SetActive(true);
+
+        // Animate PennBoy logo upwards, letter by letter, fading in
+        foreach (var (rectTrans, canvasGroup) in letterAttrs) {
+            var initPos = rectTrans.anchoredPosition;
+            var finalPos = new Vector2(rectTrans.anchoredPosition.x, 78);
+
+            StartCoroutine(Anim.Animate(0.05f, t => {
+                canvasGroup.alpha = t;
+            }));
+
+            StartCoroutine(Anim.Animate(0.5f, t => {
+                rectTrans.anchoredPosition = Vector2.Lerp(initPos, finalPos, Easing.EaseInOutExpo(t));
+            }));
+
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        // Fade in UPGRADE logo
+        var upgradeCanvasGroup = upgradeLogo.GetComponent<CanvasGroup>();
+        yield return Anim.Animate(0.3f, t => {
+            upgradeCanvasGroup.alpha = t;
+        });
     }
 }
